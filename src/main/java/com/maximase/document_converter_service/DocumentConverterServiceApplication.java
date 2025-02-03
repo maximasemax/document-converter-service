621b8e0e8b1dc9b8c4f47c2bf7dbd4896e5ddac5
maximase <armolm3@gmai9l.com>
2025-02-04 01:31:28 +0400

    0.1

---
+++ src/main/java/com/maximase/document_converter_service/DocumentConverterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maximase.document_converter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConverterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentConverterServiceApplication.class, args);
+	}
+
+}
